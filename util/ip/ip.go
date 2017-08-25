8e09c0aacfa77dc9e66474858f6512cfacd9a286
Yulin Chen <yulin.chen.uw@gmail.com>
2017-08-25 00:34:13 -0700

    Implement uint32 array reprentation for ip address of both IPv4 and IPv6 to enable fast bit level calculation

--- util/ip/ip.go
+++ util/ip/ip.go
@@ -9,11 +9,77 @@ import (
 	"net"
 )
 
+// IP address uint32 components count.
+const (
+	IPv4Uint32Count = 1
+	IPv6Uint32Count = 4
+
+	BitsPerUint32 = 32
+)
+
+// NetworkNumber represents an IP address using uint32 as internal storage.
+// IPv4 usings 1 uint32, while IPv6 uses 4 uint32.
+type NetworkNumber []uint32
+
+// NewNetworkNumber returns a equivalent NetworkNumber to given IP address,
+// return nil if ip is neither IPv4 nor IPv6.
+func NewNetworkNumber(ip net.IP) NetworkNumber {
+	if ip == nil {
+		return nil
+	}
+	coercedIP := ip.To4()
+	parts := 1
+	if coercedIP == nil {
+		coercedIP = ip.To16()
+		parts = 4
+	}
+	if coercedIP == nil {
+		return nil
+	}
+	nn := make(NetworkNumber, parts)
+	for i := 0; i < parts; i++ {
+		idx := i * net.IPv4len
+		nn[i] = binary.BigEndian.Uint32(coercedIP[idx : idx+net.IPv4len])
+	}
+	return nn
+}
+
+// ToV4 returns ip address if ip is IPv4, returns nil otherwise.
+func (n NetworkNumber) ToV4() NetworkNumber {
+	if len(n) != IPv4Uint32Count {
+		return nil
+	}
+	return n
+}
+
+// ToV6 returns ip address if ip is IPv6, returns nil otherwise.
+func (n NetworkNumber) ToV6() NetworkNumber {
+	if len(n) != IPv6Uint32Count {
+		return nil
+	}
+	return n
+}
+
+// Bit returns uint32 representing the bit value at given position, e.g.,
+// "128.0.0.0" has bit value of 1 at position 31, and 0 for positions 30 to 0.
+func (n NetworkNumber) Bit(position uint) (uint32, error) {
+	if int(position) > len(n)*BitsPerUint32-1 {
+		return 0, ErrInvalidBitPosition
+	}
+	idx := len(n) - 1
+	for ; position >= BitsPerUint32; position -= BitsPerUint32 {
+		idx--
+	}
+	lShift := position
+	mask := uint32(1) << lShift
+	return (n[idx] & mask) >> lShift, nil
+}
+
 // ErrNotIPv4Error is returned when IPv4 operations is performed on IPv6.
 var ErrNotIPv4Error = fmt.Errorf("IP is not IPv4")
 
-// ErrBitsNotValid is returned when bits requested is not valid.
-var ErrBitsNotValid = fmt.Errorf("bits requested not valid")
+// ErrInvalidBitPosition is returned when bits requested is not valid.
+var ErrInvalidBitPosition = fmt.Errorf("bit position not valid")
 
 const ipv4BitLength = 32
 
@@ -40,14 +106,14 @@ func Uint32ToIPv4(nn uint32) net.IP {
 // 		"128.0.0.0" has bit value of 1 at the 31th bit.
 func IPv4BitsAsUint(ip uint32, position uint8, numberOfBits uint8) (uint32, error) {
 	if numberOfBits == 0 || numberOfBits > ipv4BitLength || position > ipv4BitLength-1 {
-		return 0, ErrBitsNotValid
+		return 0, ErrInvalidBitPosition
 	}
 	if numberOfBits-1 > position {
-		return 0, ErrBitsNotValid
+		return 0, ErrInvalidBitPosition
 	}
-	shiftLeft := position - (numberOfBits - 1)
-	mask := (uint32(1)<<numberOfBits - 1) << shiftLeft
-	return (ip & mask) >> shiftLeft, nil
+	lShift := position - (numberOfBits - 1)
+	mask := (uint32(1)<<numberOfBits - 1) << lShift
+	return (ip & mask) >> lShift, nil
 }
 
 // NextIP returns the next sequential ip.
--- util/ip/ip_test.go
+++ util/ip/ip_test.go
@@ -7,6 +7,95 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestNewNetworkNumber(t *testing.T) {
+	cases := []struct {
+		ip   net.IP
+		nn   NetworkNumber
+		name string
+	}{
+		{nil, nil, "nil input"},
+		{net.IP([]byte{1, 1, 1, 1, 1}), nil, "bad input"},
+		{net.ParseIP("128.0.0.0"), NetworkNumber([]uint32{2147483648}), "IPv4"},
+		{
+			net.ParseIP("2001:0db8::ff00:0042:8329"),
+			NetworkNumber([]uint32{536939960, 0, 65280, 4358953}),
+			"IPv6",
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.nn, NewNetworkNumber(tc.ip))
+		})
+	}
+}
+
+func TestNetworkNumberAssertion(t *testing.T) {
+	cases := []struct {
+		ip   NetworkNumber
+		to4  NetworkNumber
+		to6  NetworkNumber
+		name string
+	}{
+		{NetworkNumber([]uint32{1}), NetworkNumber([]uint32{1}), nil, "is IPv4"},
+		{NetworkNumber([]uint32{1, 1, 1, 1}), nil, NetworkNumber([]uint32{1, 1, 1, 1}), "is IPv6"},
+		{NetworkNumber([]uint32{1, 1}), nil, nil, "is invalid"},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.to4, tc.ip.ToV4())
+			assert.Equal(t, tc.to6, tc.ip.ToV6())
+		})
+	}
+}
+
+func TestNetworkNumberBit(t *testing.T) {
+	cases := []struct {
+		ip   NetworkNumber
+		ones map[uint]bool
+		name string
+	}{
+		{NewNetworkNumber(net.ParseIP("128.0.0.0")), map[uint]bool{31: true}, "128.0.0.0"},
+		{NewNetworkNumber(net.ParseIP("1.1.1.1")), map[uint]bool{0: true, 8: true, 16: true, 24: true}, "1.1.1.1"},
+		{NewNetworkNumber(net.ParseIP("8000::")), map[uint]bool{127: true}, "8000::"},
+		{NewNetworkNumber(net.ParseIP("8000::8000")), map[uint]bool{127: true, 15: true}, "8000::8000"},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			for i := uint(0); i < uint(len(tc.ip)*BitsPerUint32); i++ {
+				bit, err := tc.ip.Bit(i)
+				assert.NoError(t, err)
+				if _, isOne := tc.ones[i]; isOne {
+					assert.Equal(t, uint32(1), bit)
+				} else {
+					assert.Equal(t, uint32(0), bit)
+				}
+			}
+		})
+	}
+}
+
+func TestNetworkNumberBitError(t *testing.T) {
+	cases := []struct {
+		ip       NetworkNumber
+		position uint
+		err      error
+		name     string
+	}{
+		{NewNetworkNumber(net.ParseIP("128.0.0.0")), 0, nil, "IPv4 index in bound"},
+		{NewNetworkNumber(net.ParseIP("128.0.0.0")), 31, nil, "IPv4 index in bound"},
+		{NewNetworkNumber(net.ParseIP("128.0.0.0")), 32, ErrInvalidBitPosition, "IPv4 index out of bounds"},
+		{NewNetworkNumber(net.ParseIP("8000::")), 0, nil, "IPv6 index in bound"},
+		{NewNetworkNumber(net.ParseIP("8000::")), 127, nil, "IPv6 index in bound"},
+		{NewNetworkNumber(net.ParseIP("8000::")), 128, ErrInvalidBitPosition, "IPv6 index out of bounds"},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := tc.ip.Bit(tc.position)
+			assert.Equal(t, tc.err, err)
+		})
+	}
+}
+
 func TestIPv4ToUint32(t *testing.T) {
 	cases := []struct {
 		ip          string
@@ -54,10 +143,10 @@ func TestIPv4BitsAsUint(t *testing.T) {
 		expectedBits uint32
 		expectedErr  error
 	}{
-		{"0.0.0.1", 1, 0, 0, 0, ErrBitsNotValid},
-		{"0.0.0.1", 1, 0, 2, 0, ErrBitsNotValid},
-		{"0.0.0.1", 1, 0, 33, 0, ErrBitsNotValid},
-		{"0.0.0.1", 1, 32, 1, 0, ErrBitsNotValid},
+		{"0.0.0.1", 1, 0, 0, 0, ErrInvalidBitPosition},
+		{"0.0.0.1", 1, 0, 2, 0, ErrInvalidBitPosition},
+		{"0.0.0.1", 1, 0, 33, 0, ErrInvalidBitPosition},
+		{"0.0.0.1", 1, 32, 1, 0, ErrInvalidBitPosition},
 		{"0.0.0.1", 1, 0, 1, 1, nil},
 		{"0.0.0.1", 1, 1, 2, 1, nil},
 		{"0.0.0.1", 1, 2, 1, 0, nil},
